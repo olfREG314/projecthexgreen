528a13020d07039a25ccd19440d6c081ba48098a
olfreg314 <olfreg314@pop-os.localdomain>
2022-01-23 22:39:42 +0530

    added google maps but some errors

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,17 +1,48 @@
 <template>
-  abc
-  <div id="map"></div>
+  <div id="anc" class="map" style="height: 100vh; width: 100vw" v-cloak></div>
 </template>
 <script>
-export default {};
-let map;
+// /* eslint-disable no-undef */
+import { Loader } from "@googlemaps/js-api-loader";
+import { onMounted } from "vue";
+import window from "window";
+//window is a global var
+const google = window.google;
+// import { useGeolocation } from "@/components/useGeolocation";
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
-initMap;
+const GOOGLE_MAPS_API_KEY = "AIzaSyBFBMZrSA1l0h6rUR5ywIt6zlxLBBuD0ag";
+export default {
+  data() {
+    return {};
+  },
+  setup() {
+    const loader = new Loader({ apiKey: GOOGLE_MAPS_API_KEY });
+
+    //getting my location
+    // const { coords } = useGeolocation();
+    // const currPos = computed(() => ({
+    //   lat: coords.value.latitude,
+    //   lng: coords.value.longitude,
+    // }));
+    // console.log(currPos.value);
+    //load map
+    onMounted(async () => {
+      await loader.load().then(() => {
+        new google.maps.Map(document.getElementById("anc"), {
+          center: { lat: -34.397, lng: 150.644 },
+          zoom: 8,
+        });
+      });
+    });
+  },
+};
 </script>
-<style scoped></style>
+<style>
+[v-cloak] {
+  display: none;
+}
+/* .map {
+  width: 100%;
+  height: 100%;
+} */
+</style>
---
+++ src/components/useGeolocation.js
@@ -0,0 +1,26 @@
+/** @format */
+
+import { onUnmounted, onMounted, ref } from "vue";
+
+export function useGeolocation() {
+  const coords = ref({ latitude: 0, longitude: 0 });
+  const isSupported = "navigator" in window && "geolocation" in navigator;
+
+  let watcher = null;
+  onMounted(() => {
+    if (isSupported) {
+      watcher = navigator.geolocation.watchPosition(
+        (position) => (coords.value = position.coords)
+      );
+      console.log("its in if");
+    }
+  });
+  onUnmounted(() => {
+    if (watcher) {
+      navigator.geolocation.clearWatch(watcher);
+      console.log("its in unMOunted");
+    }
+  });
+
+  return { coords, isSupported };
+}
